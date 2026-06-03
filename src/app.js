bce8d4a5b228098adf931f17bce5e0b850fc77e4
girnarpaint <girnarpaint@gmail.com>
2026-06-03 23:20:26 +0530

    final

--- src/app.js
+++ src/app.js
@@ -2,8 +2,8 @@ const express=require("express")
 const cors = require("cors"); 
 
 const app=express()
-app.use(cors({             // add this block
-  origin: "http://localhost:5173",
+app.use(cors({             
+  origin: "https://away-frontend-tau.vercel.app/",
   credentials: true
 }));
 app.use(express.json());
